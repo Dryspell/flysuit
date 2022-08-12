273f8198e5634a0979d8a61eee2469cd95e3c62c
Dryspell <Nhwinslow@gmail.com>
2022-08-11 23:08:25 -0400

    Functional Search Proxy

--- pages/api/hubspot/[entity]/search.ts
+++ pages/api/hubspot/[entity]/search.ts
@@ -8,10 +8,30 @@ export default async function handler(
   if (!['POST'].includes(req.method as string))
     return res.status(405).json({ message: `Method ${req.method} Not Allowed` })
 
+  console.log(`Query:`, JSON.stringify(req.query))
   const entityPlural = req.query.entity as string
   const queryLimit = (req.query.limit as string) || '100'
+  const requestParams = Object.entries(req.query).filter(([key, value]) => {
+    return !['entity', 'limit'].includes(key)
+  })
+  const body = !Object.keys(req.body).includes('filterGroups')
+    ? {
+        filterGroups: [
+          {
+            filters: requestParams.slice(0, 3).map(([key, value]) => {
+              return {
+                propertyName: key,
+                operator: 'IN',
+                values: Array.isArray(value) ? value : [value],
+              }
+            }),
+          },
+        ],
+      }
+    : req.body.filterGroups
 
-  const searchResults = await searchHubspot(entityPlural, req.body, queryLimit)
+  console.log(`Search Body:`, JSON.stringify(body))
+  const searchResults = await searchHubspot(entityPlural, body, queryLimit)
 
   return res.status(200).json(searchResults)
 }
