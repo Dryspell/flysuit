eead4f4d18f76ab9fa6cb3308cb0a0ccc0f78096
Dryspell <Nhwinslow@gmail.com>
2022-09-18 14:43:11 -0400

    Scrape changes

--- pages/api/cheerio/scrape.ts
+++ pages/api/cheerio/scrape.ts
@@ -5,7 +5,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const scrape = async (url: string, selector: string) => {
+  const scrape = async (url: string, selector: string, attribute?: string) => {
     try {
       const response = await fetch(url).then((res) => res.text())
       const $ = cheerio.load(response)
@@ -13,7 +13,11 @@ export default async function handler(
       console.log(`Received ${results.text().length} characters of text`)
 
       const resultsArray: any[] = []
-      results.each((i) => resultsArray.push($(results[i]).text()))
+      attribute
+        ? results.each((i) =>
+            resultsArray.push($(results[i]).attr(attribute as string))
+          )
+        : results.each((i) => resultsArray.push($(results[i]).text()))
       return resultsArray
     } catch (err: any) {
       console.log(`Received invalid inputs: ${url} and ${selector}`)
@@ -24,17 +28,47 @@ export default async function handler(
   const selector = (req.query.selector as string) || 'div'
   const url = req.query.url as string
 
-  const flysuit = await scrape(
-    'https://flysuit.vercel.app/test-portal/mantine-page',
-    'div'
-  )
-  const wikipedia = await scrape('https://en.wikipedia.org/wiki/Boston', 'p')
+  const examples = [
+    {
+      exampleName: 'redditImgs1',
+      url: 'https://www.reddit.com/r/pics/',
+      selector: 'img[alt="Post image"]',
+      attribute: 'src',
+    },
+    {
+      exampleName: 'redditImgs2',
+      url: 'https://www.reddit.com/r/pics/search/?q=chicken',
+      selector: 'a',
+      attribute: 'href',
+    },
+    {
+      exampleName: 'flysuit',
+      url: 'https://flysuit.vercel.app/test-portal/mantine-page',
+      selector: 'div',
+    },
+    {
+      exampleName: 'wikipedia',
+      url: 'https://en.wikipedia.org/wiki/Boston',
+      selector: 'p',
+    },
+  ]
+  const getExampleResults = async () => {
+    const results: any[] = []
+    for (const example of examples) {
+      const result = await scrape(
+        example.url,
+        example.selector,
+        example.attribute
+      )
+      results.push({ ...example, result })
+    }
+    return results
+  }
 
   const results = url
-    ? await scrape(url, selector)
+    ? await scrape(url, selector, req.query.attribute as string)
     : {
-        flysuit,
-        // wikipedia
+        examples: await getExampleResults(),
       }
   return res.status(200).json(results)
 }
