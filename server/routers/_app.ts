f0a6f4b0495f1aab8bcbf2cef7dbd57bbc75302c
Dryspell <Nhwinslow@gmail.com>
2022-10-09 20:45:09 -0400

    Switched to TRPC?

--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -1,6 +1,7 @@
 import superjson from 'superjson'
 import { createRouter } from '../create-router'
 import { commentRouter } from './comment'
+import { newsRouter } from './news'
 // import { hubspotRouter } from './hubspot'
 import { postRouter } from './post'
 import { userRouter } from './user'
@@ -10,6 +11,7 @@ export const appRouter = createRouter()
   .merge('post.', postRouter)
   .merge('comment.', commentRouter)
   .merge('user.', userRouter)
+  .merge('news.', newsRouter)
 // .merge('hubspot.', hubspotRouter)
 
 export type AppRouter = typeof appRouter
