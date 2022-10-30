5af7c38158d997de7527bbd5641ff5d6b5158e71
Dryspell <Nhwinslow@gmail.com>
2022-10-30 12:09:26 -0400

    More Sandpack and Sandpack Ace Editor

--- pages/test-portal/sandpackAce.tsx
+++ pages/test-portal/sandpackAce.tsx
@@ -5,7 +5,8 @@ import {
   SandpackPreview,
 } from '@codesandbox/sandpack-react'
 import { atomDark } from '@codesandbox/sandpack-themes'
-import { markdown, markdownLanguage } from '@codemirror/lang-markdown'
+
+import dynamic from 'next/dynamic'
 
 const code = `export default function App() {
   return <h1>Hello Sandpack</h1>
@@ -13,30 +14,20 @@ const code = `export default function App() {
 
 const md = `# Hello Sandpack`
 
+const Ace = dynamic(() => import('./ace'), { ssr: false })
+
 export default function Page() {
   return (
     <SandpackProvider
       template="react"
       theme={atomDark}
       files={{
-        '/App.js': code,
+        // '/App.js': code,
         '/markdown.md': md,
       }}
     >
       <SandpackLayout>
-        <SandpackCodeEditor
-          showLineNumbers={true}
-          showTabs
-          showInlineErrors
-          wrapContent
-          // additionalLanguages={[
-          //   {
-          //     name: 'markdown',
-          //     extensions: ['md'],
-          //     language: markdown(),
-          //   },
-          // ]}
-        />
+        <Ace />
         <SandpackPreview />
       </SandpackLayout>
     </SandpackProvider>
