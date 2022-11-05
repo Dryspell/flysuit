78f5ed0aef91c135ef2c808d1490b2f76f130bf2
Dryspell <Nhwinslow@gmail.com>
2022-11-05 12:26:28 -0400

    Mantine Navbars

--- pages/test-portal/mantine-page.tsx
+++ pages/test-portal/mantine-page.tsx
@@ -13,6 +13,8 @@ import * as React from 'react'
 import { LanguagePicker } from '@/components/MantineComponents/LanguagePicker'
 import { GradientSegmentedControl } from '@/components/MantineComponents/GradientSegementedControl'
 import { HeaderSearch } from '@/components/MantineComponents/HeaderWithSearch'
+import { DoubleNavbar } from '@/components/MantineComponents/DoubleNavbar'
+import { NavbarNested } from '@/components/MantineComponents/NestedNavbar'
 
 const PRIMARY_COL_HEIGHT = 300
 const links = [
@@ -41,44 +43,59 @@ export default function Page() {
   return (
     <>
       <HeaderSearch links={links} />
-      <Container my="md">
-        <Container my="md">
-          <SimpleGrid
-            cols={2}
-            spacing="md"
-            breakpoints={[{ maxWidth: 'sm', cols: 1 }]}
-          >
-            <LanguagePicker />
-            <GradientSegmentedControl />
-          </SimpleGrid>
-        </Container>
-        <SimpleGrid
-          cols={2}
-          spacing="md"
-          breakpoints={[{ maxWidth: 'sm', cols: 1 }]}
-        >
-          <BadgeCard
-            image={BadgeData.image}
-            title={BadgeData.title}
-            description={BadgeData.description}
-            country={BadgeData.description}
-            badges={BadgeData.badges}
-          />
-          <Grid gutter="md">
-            <Grid.Col>{/* <StatsControls /> */}</Grid.Col>
-            <Grid.Col span={6}>
-              <StatsCard />
-            </Grid.Col>
-            <Grid.Col span={6}>
-              <StatsSegments
-                total={StatsSegmentsData.total}
-                diff={StatsSegmentsData.diff}
-                data={StatsSegmentsData.data}
+      {/* <SimpleGrid
+        cols={2}
+        spacing="md"
+        breakpoints={[{ maxWidth: 'sm', cols: 1 }]}
+      >
+        <NavbarNested /> */}
+      <Grid>
+        <Grid.Col span={3}>
+          {/* <NavbarNested /> */}
+          <div>Drawer</div>
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <Container my="md">
+            <Container my="md">
+              <SimpleGrid
+                cols={2}
+                spacing="md"
+                breakpoints={[{ maxWidth: 'sm', cols: 1 }]}
+              >
+                <LanguagePicker />
+                <GradientSegmentedControl />
+              </SimpleGrid>
+            </Container>
+            <SimpleGrid
+              cols={2}
+              spacing="md"
+              breakpoints={[{ maxWidth: 'sm', cols: 1 }]}
+            >
+              <BadgeCard
+                image={BadgeData.image}
+                title={BadgeData.title}
+                description={BadgeData.description}
+                country={BadgeData.description}
+                badges={BadgeData.badges}
               />
-            </Grid.Col>
-          </Grid>
-        </SimpleGrid>
-      </Container>
+              <Grid gutter="md">
+                <Grid.Col>{/* <StatsControls /> */}</Grid.Col>
+                <Grid.Col span={6}>
+                  <StatsCard />
+                </Grid.Col>
+                <Grid.Col span={6}>
+                  <StatsSegments
+                    total={StatsSegmentsData.total}
+                    diff={StatsSegmentsData.diff}
+                    data={StatsSegmentsData.data}
+                  />
+                </Grid.Col>
+              </Grid>
+            </SimpleGrid>
+          </Container>
+          {/* </SimpleGrid> */}
+        </Grid.Col>
+      </Grid>
     </>
   )
 }
