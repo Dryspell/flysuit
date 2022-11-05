9e1887996d756cd44d4dd068e65a35431dfa58fa
Dryspell <Nhwinslow@gmail.com>
2022-11-05 12:05:05 -0400

    Added Header to Mantine Page

--- pages/test-portal/mantine-page.tsx
+++ pages/test-portal/mantine-page.tsx
@@ -12,51 +12,73 @@ import {
 import * as React from 'react'
 import { LanguagePicker } from '@/components/MantineComponents/LanguagePicker'
 import { GradientSegmentedControl } from '@/components/MantineComponents/GradientSegementedControl'
+import { HeaderSearch } from '@/components/MantineComponents/HeaderWithSearch'
 
 const PRIMARY_COL_HEIGHT = 300
+const links = [
+  {
+    link: '/about',
+    label: 'Features',
+  },
+  {
+    link: '/pricing',
+    label: 'Pricing',
+  },
+  {
+    link: '/learn',
+    label: 'Learn',
+  },
+  {
+    link: '/community',
+    label: 'Community',
+  },
+]
 
 export default function Page() {
   const theme = useMantineTheme()
   const SECONDARY_COL_HEIGHT = PRIMARY_COL_HEIGHT / 2 - theme.spacing.md / 2
 
   return (
-    <Container my="md">
+    <>
+      <HeaderSearch links={links} />
       <Container my="md">
+        <Container my="md">
+          <SimpleGrid
+            cols={2}
+            spacing="md"
+            breakpoints={[{ maxWidth: 'sm', cols: 1 }]}
+          >
+            <LanguagePicker />
+            <GradientSegmentedControl />
+          </SimpleGrid>
+        </Container>
         <SimpleGrid
           cols={2}
           spacing="md"
           breakpoints={[{ maxWidth: 'sm', cols: 1 }]}
         >
-          <LanguagePicker />
-          <GradientSegmentedControl />
+          <BadgeCard
+            image={BadgeData.image}
+            title={BadgeData.title}
+            description={BadgeData.description}
+            country={BadgeData.description}
+            badges={BadgeData.badges}
+          />
+          <Grid gutter="md">
+            <Grid.Col>{/* <StatsControls /> */}</Grid.Col>
+            <Grid.Col span={6}>
+              <StatsCard />
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <StatsSegments
+                total={StatsSegmentsData.total}
+                diff={StatsSegmentsData.diff}
+                data={StatsSegmentsData.data}
+              />
+            </Grid.Col>
+          </Grid>
         </SimpleGrid>
       </Container>
-      <SimpleGrid
-        cols={2}
-        spacing="md"
-        breakpoints={[{ maxWidth: 'sm', cols: 1 }]}
-      >
-        <BadgeCard
-          image={BadgeData.image}
-          title={BadgeData.title}
-          description={BadgeData.description}
-          country={BadgeData.description}
-          badges={BadgeData.badges}
-        />
-        <Grid gutter="md">
-          <Grid.Col>{/* <StatsControls /> */}</Grid.Col>
-          <Grid.Col span={6}>
-            <StatsCard />
-          </Grid.Col>
-          <Grid.Col span={6}>
-            <StatsSegments
-              total={StatsSegmentsData.total}
-              diff={StatsSegmentsData.diff}
-              data={StatsSegmentsData.data}
-            />
-          </Grid.Col>
-        </Grid>
-      </SimpleGrid>
-    </Container>
+    </>
   )
 }
