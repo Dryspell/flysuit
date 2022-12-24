7237e069a2437bfbc6fe123a2f7c70d83142ae28
Noah <NhWinslow@gmail.com>
2022-12-24 09:04:04 -0700

    Moved Around tests

---
+++ tests/postCustomSchema.spec.ts
@@ -0,0 +1,51 @@
+import { test, expect } from "@playwright/test"
+
+test("PostCustomSchema", async ({ page }) => {
+	const pokemonSchema = {
+		name: "pokemon",
+		labels: {
+			singular: "Pokemon",
+			plural: "Pokemons",
+		},
+		properties: [
+			{
+				name: "name",
+				label: "Name",
+				type: "string",
+				fieldType: "text",
+			},
+			{
+				name: "height",
+				label: "Height",
+				type: "string",
+				fieldType: "text",
+			},
+			{
+				name: "weight",
+				label: "Weight",
+				type: "string",
+				fieldType: "text",
+			},
+			{
+				name: "types",
+				label: "Types",
+				type: "string",
+				fieldType: "text",
+			},
+		],
+		associatedObjects: ["CONTACT"],
+		primaryDisplayProperty: "id",
+	}
+
+	const postCustomSchema = await page.request
+		.post(`https://api.hubapi.com/crm/v3/schemas`, {
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${process.env.HUBSPOT_TEST_API_KEY}`,
+			},
+			data: { ...pokemonSchema },
+		})
+		.then((res) => res.json())
+
+	console.log(`postCustomSchema`, postCustomSchema)
+})
