a6db6d815c0b0e2a54f3c7aa1d2e99c21c345fd2
Dryspell <Nhwinslow@gmail.com>
2023-01-07 12:39:37 -0500

    Modified eslint and created scene with floor

--- pages/test-portal/three.tsx
+++ pages/test-portal/three.tsx
@@ -1,8 +1,21 @@
 import React from "react"
 import css from "../../styles/Three.module.css"
+import { Canvas } from "@react-three/fiber"
+import Floor from "@/components/Three/Floor"
 
 // This page is following the tutorial https://dev.to/hnicolus/how-to-use-threejs-in-react-nextjs-4120
 
 export default function Three() {
-	return <div className={css.scene}></div>
+	return (
+		<div className={css.scene}>
+			<Canvas
+				shadows={true}
+				className={css.canvas}
+				camera={{ position: [-6, 7, 7] }}
+			>
+				<ambientLight color={"white"} intensity={0.3} />
+				<Floor position={[0, -1, 0]} />
+			</Canvas>
+		</div>
+	)
 }
