10d77760fd616ac23330970c9551e8cd3d6d35f4
Dryspell <Nhwinslow@gmail.com>
2023-01-07 11:47:16 -0500

    Initialized React Three Fiber

---
+++ pages/test-portal/three.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import css from "../../styles/Three.module.css"
+
+// This page is following the tutorial https://dev.to/hnicolus/how-to-use-threejs-in-react-nextjs-4120
+
+export default function Three() {
+	return <div className={css.scene}></div>
+}
